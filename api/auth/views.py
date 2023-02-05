228951ee25928264dc3baa5ea8083cdff336e5d0
elijah obara <elijahobara357@gmail.com>
2023-02-05 09:15:27 +0300

    Added all routes

--- api/auth/views.py
+++ api/auth/views.py
@@ -2,8 +2,24 @@ from flask_restx import Namespace,Resource
 
 auth_namespace=Namespace('auth', description="a namespace for authentication")
 
-@auth_namespace.route('/')
-class HelloAuth(Resource):
+@auth_namespace.route('/signup')
+class SignUp(Resource):
     
-    def get(self):
-        return {"message": "hello auth"}
+    def post(self):
+        
+        """
+            create a new user account
+        """
+        pass
+    
+    
+@auth_namespace.route('/login')
+class Login(Resource):
+    
+    def post(self):
+        
+        """
+            Generate a JWT pair
+        """
+        
+        pass
--- api/orders/views.py
+++ api/orders/views.py
@@ -3,8 +3,76 @@ from flask_restx import Resource,Namespace
 order_namespace=Namespace('orders',description="Namespace for orders")
 
 
-@order_namespace.route('/')
-class HelloOrders(Resource):
+@order_namespace.route('/orders')
+class OrderGetCreate(Resource):
     
     def get(self):
-        return {"message":"Hello orders"}
+        
+        """
+            Get all orders
+        """
+        pass
+    
+    def post(self):
+        
+        """
+            place a new order
+        """
+        pass
+    
+
+@order_namespace.route('/order/<int:order_id>')
+class GetUpdateDelete(Resource):
+    
+    def get(self, order_id):
+        
+        """ 
+            Retrieve an order by id
+        """
+        
+        pass
+    
+    def put(self, order_id):
+        
+        """ 
+            Update an order with id
+        """
+        
+        pass
+    
+    def delete(self, order_id):
+        """
+        Delete an order with id
+        
+        """
+        pass        
+    
+
+@order_namespace.route('/user/<int:user_id>/order/<int:order_id>/')
+class GetSpecificOrderByUser(Resource):
+    def get(self,user_id,order_id):
+        """
+        Get a user's specific order
+        
+        """
+        pass
+    
+@order_namespace.route('/user/<int:user_id>/ordres')
+class UserOrders(Resource):
+    
+    def get(self, user_id):
+        """
+        Get all orders by a specific user
+        
+        """ 
+        pass  
+    
+@order_namespace.route('/order/status/<int:order_id>') 
+class UpdateOrderStatus(Resource):
+    
+    def patch(self,order_id):
+        """
+        Update order status
+        
+        """ 
+        pass    
