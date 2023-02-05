e56e15fa470c25e79c6828ff1618f4d2e5080b34
elijah obara <elijahobara357@gmail.com>
2023-02-05 22:59:21 +0300

    Crated serializer models

--- api/auth/views.py
+++ api/auth/views.py
@@ -1,16 +1,59 @@
-from flask_restx import Namespace,Resource
+from flask_restx import Namespace,Resource,fields
+from flask import request
+from ..models.users import User
+from werkzeug.security import generate_password_hash,check_password_hash
+from http import HTTPStatus
 
 auth_namespace=Namespace('auth', description="a namespace for authentication")
+
+
+signup_model=auth_namespace.model(
+    'SignUp',{
+        'id':fields.Integer(),
+        'username':fields.String(required=True,description="A username"),
+        'email':fields.String(required=True,description="An email"),
+        'password':fields.String(required=True,description="A password"),
+        
+    }
+)
+
+user_model=auth_namespace.model(
+    'User', {
+        'id':fields.Integer(),
+        'username':fields.String(required=True,description="A username"),
+        'email':fields.String(required=True,description="An email"),
+        'password_hash':fields.String(required=True,description="A password"),
+        'is_active':fields.Boolean(description="This shows that user is active"),
+        'is_staff':fields.Boolean(description="This shows of use is staff")
+    }
+)
+
 
 @auth_namespace.route('/signup')
 class SignUp(Resource):
     
+    @auth_namespace.expect(signup_model)
+    @auth_namespace.marshal_with(user_model)
     def post(self):
         
         """
             create a new user account
         """
-        pass
+        
+        data = request.get_json()
+        
+        
+        new_user=User(
+            username=data.get('username'),
+            email=data.get('email'),
+            password_hash=generate_password_hash(data.get('password'))
+        )
+        
+        new_user.save()
+        
+        return new_user ,HTTPStatus.CREATED
+        
+        
     
     
 @auth_namespace.route('/login')
